fe5194b7eb6dd94225a146f46e14cdefb0eb8c36
pkosiniak <37831217+pkosiniak@users.noreply.github.com>
2021-02-07 13:46:40 +0100

    Popup: add toggle, autoToggle OfferRow buttons + styling

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,14 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MESSAGE_TYPE, OriginType, SenderType } from '../../types/message';
 import { messageSender } from '../../utils/messages/messageSender';
 import { useUUID } from '../../hooks/useUUID';
-// import './Popup.css';
+import './Popup.css';
 import * as P from './parts';
 import TextInput from '../../Components/Input/TextInput/TextInput';
-import Box from '../../Components/Box/Box';
-import Button from '../../Components/Button/Button';
 import CopyButton from './CopyButton/CopyButton';
-
+import { messageListener } from '../../utils/messages/messageListener';
+import { SendToOption } from '../../utils/messages/types';
+import { getSettingsListener } from '../../settings/settingsListener';
+import { SettingsType } from '../../types/settings';
+import { usePrevProps } from '../../hooks/usePrevProps';
+import CopyRow from './modules/CopyRow';
 
 const useSender = (originType: OriginType): SenderType => {
    const UUID = useUUID();
@@ -18,32 +21,86 @@ const useSender = (originType: OriginType): SenderType => {
    };
 };
 
-const Popup = () => {
-   const sender = useSender(OriginType.Popup);
-   const sendGetOfferInfo = () => messageSender(
-      MESSAGE_TYPE.GET_OFFER_INFO,
-      sender,
-      void 0,
-      { toActiveTab: true },
-   );
+const code = 'document.getElementById("extensionContainer")?.style.display';
 
-   const sendShowOfferInfoRow = () => messageSender(
-      MESSAGE_TYPE.SHOW_OFFER_INFO_ROW,
-      sender,
-      void 0,
-      { toActiveTab: true },
-   );
+const Popup = () => {
+
+   const [isShowOffer, setIsShowOffer] = useState(false);
+   chrome.tabs.query({ active: true, currentWindow: true }, result => {
+      const id = result[0].id;
+      id && chrome.tabs.executeScript({ code }, result => {
+         setIsShowOffer(!!result[0] && result[0] === 'block');
+      });
+   });
+
+   const sender = useSender(OriginType.Popup);
+
+   const sendShowOfferInfoRow = (prev: boolean) => {
+      messageSender(
+         MESSAGE_TYPE.TOGGLE_OFFER_INFO_ROW,
+         sender,
+         !prev,
+         { toActiveTab: true },
+      );
+      return !prev;
+   };
+
+   const [settings, setSettings] = useState<SettingsType>();
+   const prevSettings = usePrevProps(settings);
+   useEffect(() => {
+      return getSettingsListener((settings) => setSettings(settings), sender);
+   }, []);
+
+   const sendAutoToggle = (
+      current: SettingsType,
+   ) => {
+      messageSender(
+         MESSAGE_TYPE.SETTINGS_AUTO_TOGGLE,
+         sender,
+         !current.autoToggle,
+         SendToOption.Runtime,
+      );
+      setSettings({ ...current, autoToggle: !current.autoToggle });
+   };
 
    const linkedInRef = useRef<HTMLInputElement>(null);
    const githubRef = useRef<HTMLInputElement>(null);
 
    const [linkedInCopy, setLinkedInCopy] = useState(false);
-   const [githubCopy, seGithubCopy] = useState(false);
+   const [githubCopy, setGithubCopy] = useState(false);
 
+   console.log('settings, prevSettings', settings, prevSettings);
    return (
       <P.Wrapper v>
-         <P.ActionButton text={'SHOW OFFER ROW'} onClick={() => sendShowOfferInfoRow()} />
-         <Box h>
+         <P.LineWrapper>
+            <P.StyledToggleSwitch
+               label={'TOGGLE OFFER ROW'}
+               inputPosition={'after'}
+               isChecked={isShowOffer}
+               setIsChecked={() => setIsShowOffer(sendShowOfferInfoRow)}
+               wrapperProps={{ className: 'toggleSwitchWrapper' }}
+               labelProps={{ className: 'toggleSwitchLabel' }}
+            />
+         </P.LineWrapper>
+         <P.LineWrapper>
+            <P.StyledToggleSwitch
+               label={'AUTO TOGGLE OFFER ROW'}
+               isChecked={!!settings?.autoToggle}
+               setIsChecked={() => settings && sendAutoToggle(settings)}
+               wrapperProps={{ className: 'toggleSwitchWrapper' }}
+               labelProps={{ className: 'toggleSwitchLabel' }}
+            />
+         </P.LineWrapper>
+         <CopyRow
+            label={'LinkedIn'}
+            text={'https://www.linkedin.com/in/pawel-kosiniak/'}
+            isCopied={linkedInCopy}
+            setIsCopied={() => {
+               setLinkedInCopy(true);
+               setGithubCopy(false);
+            }}
+         />
+         {/* <P.LineWrapper h>
             <TextInput
                ref={linkedInRef}
                readOnly
@@ -55,36 +112,20 @@ const Popup = () => {
                   linkedInRef.current?.select();
                   document.execCommand('copy');
                   setLinkedInCopy(true);
-                  seGithubCopy(false);
+                  setGithubCopy(false);
                }}
                isCopied={linkedInCopy}
             />
-         </Box>
-         <Box h>
-            <TextInput
-               ref={githubRef}
-               readOnly
-               label={'GitHub'}
-               defaultValue={'https://github.com/pkosiniak'}
-            />
-            <CopyButton 
-               onClick={() => {
-                  githubRef.current?.select();
-                  document.execCommand('copy');
-                  setLinkedInCopy(false);
-                  seGithubCopy(true);
-               }}
-               isCopied={githubCopy}
-            />
-            {/* <Button
-               
-            >
-               {'📄'}
-               <span style={{ position: 'relative', top: 4, left: -12 }}>{'📄'}</span>
-               {githubCopy && <span style={{ color: '#0f0', position: 'absolute', marginLeft: -20 }}>{'✔'}</span>}
-
-            </Button> */}
-         </Box>
+         </P.LineWrapper> */}
+         <CopyRow
+            label={'GitHub'}
+            text={'https://github.com/pkosiniak'}
+            isCopied={githubCopy}
+            setIsCopied={() => {
+               setLinkedInCopy(false);
+               setGithubCopy(true);
+            }}
+         />
       </P.Wrapper>
    );
 };
