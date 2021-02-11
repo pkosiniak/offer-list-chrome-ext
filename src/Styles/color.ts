a9eb4475ea7d56de7b8f2c746faf42b70197c262
pkosiniak <37831217+pkosiniak@users.noreply.github.com>
2021-02-11 17:36:34 +0100

    add Color(Alpha) and textShadow

--- src/Styles/color.ts
+++ src/Styles/color.ts
@@ -9,15 +9,24 @@ export const color = {
    white: '#fff',
    lightest: '#eee',
    lighter: '#ccc',
-   lighter8: '#ccc8',
    light: '#aaa',
-   light8: '#aaa8',
    grayLight: '#888',
    gray: '#666',
    grayDark: '#555',
    dark: '#444',
    darker: '#333',
    darkest: '#222',
-   darkest8: '#2228',
    black: '#000',
 };
+
+const keys = Object.keys(color);
+const values = Object.values(color);
+
+type ColorAlpha = {[K in keyof typeof color]: (alpha: string) => string}
+
+export const Color: ColorAlpha = Object.fromEntries(
+   keys.map((key, index) => [
+      key,
+      (alpha: string) => values[index] + alpha,
+   ]),
+) as ColorAlpha; 
--- src/Styles/font.ts
+++ src/Styles/font.ts
@@ -1,4 +1,5 @@
 import { css } from 'styled-components';
+import { color as C } from './color';
 
 const fontCss = (size: number) => css`
    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -8,3 +9,14 @@ const fontCss = (size: number) => css`
 export const font = {
    medium: fontCss(16),
 };
+
+export const textShadow = (
+   width: number = 1,
+   color: string = C.black,
+) => css`
+   text-shadow: 
+   -${width}px 0 ${width}px ${color}, 
+   0 ${width}px ${width}px ${color}, 
+   ${width}px 0 ${width}px ${color}, 
+   0 -${width}px ${width}px ${color};
+`;
