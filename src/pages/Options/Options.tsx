3ab6fd9dfac2bdac819cd74e0e7b721d08f6eb85
pkosiniak <37831217+pkosiniak@users.noreply.github.com>
2021-02-11 17:38:53 +0100

    Options as playground

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { CellWidth } from '../../Modules/CellsCollection/modules/Cells/shared/types';
+import HeadingCell from '../../Modules/Heading/HeadingCell';
+import { SortDirection } from '../../Modules/Heading/types';
 import './Options.css';
 
 interface Props {
 }
 
-const Options: React.FC<Props> = ({  }) => {
+const Options: React.FC<Props> = ({ }) => {
    return (
       <div className="OptionsContainer">
-         SETTINGS PAGE
+         <HeadingCell
+            text="APPLICATION"
+            width={CellWidth.Medium}
+            widthRange={{
+               min: CellWidth.Small,
+               max: CellWidth.Large,
+               default: CellWidth.Medium,
+            }}
+            setFilter={() => { }}
+            sort={SortDirection.NONE}
+            setSort={() => { }}
+         />
       </div>
    );
 };
